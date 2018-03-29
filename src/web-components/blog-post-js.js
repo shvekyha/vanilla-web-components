6872b2e5099ef589d707e57f753a8bd5ecfdf7c2
hadas <hadas@xmpie.com>
2018-03-29 15:54:14 +0300

    first commit

---
+++ src/web-components/blog-post-js.js
@@ -0,0 +1,30 @@
+
+class BlogPostJSComponent extends HTMLElement{
+    constructor() {
+        super(); 
+        //var template = document.querySelector('template').content;
+        //console.log('template: '+template);
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        console.log('connectedCallback ');
+        console.log('this.template: '+this.htmlTemplate);
+        
+        //if using plain html
+        this.shadowRoot.innerHTML = this.htmlTemplate;
+    }
+    
+    get htmlTemplate() {
+        return `
+            <div id="blog-post">  
+                <h1>BlogPostJS Awesome header!!!</h1>
+                <p>Here is some really interesting paragraph content.</p>
+            </div>
+        `
+    }
+}
+
+window.customElements.define('blog-post-js', BlogPostJSComponent)
+
+  
